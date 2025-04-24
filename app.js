e62c9319e13c5428c1ad0babc3250998ab8e01f8
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2025-04-24 21:14:06 +0530

    Completed Mini-message board project

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const assestsPath = path.join(__dirname, "public");
+
+app.use(express.static(assestsPath));
+
+let id = 1;
+const messages = [
+  {
+    text: "My wifey has a fatass!",
+    user: "Hemant",
+    added: new Date(),
+    id: id++,
+  },
+  {
+    text: "My hubby loves my fatass",
+    user: "Lynn",
+    added: new Date(),
+    id: id++,
+  },
+];
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Mini-message Board", messages: messages });
+});
+
+app.get("/new", (req, res) => {
+  res.render("form", { title: "New Message", text: "Here will be the form." });
+});
+
+app.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+
+  res.redirect("/");
+});
+
+app.get("/message/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const message = messages.find(message => message.id === id);
+  res.render("message", {message: message})
+})
+
+app.listen(3000, () => {
+  console.log("Server started!");
+});
